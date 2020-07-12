3ab125708a2774a785b692945de9f4f3456b3f13
hernani <megahack9@outlook.com>
2020-07-12 13:05:51 -0300

    apk

--- android/app/src/main/java/com/jlbtu/MainApplication.java
+++ android/app/src/main/java/com/jlbtu/MainApplication.java
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          new CodePush(BuildConfig.CODEPUSH_KEY, getAppicationContext(), BuildConfig.DEBUG);
+          packages.add(new CodePush(BuildConfig.CODEPUSH_KEY, getAppicationContext(), BuildConfig.DEBUG));
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
